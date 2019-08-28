0b8d6f8f4d5a662cf8e09063686e15314522f1c4
peace710 <nothingsuper@163.com>
2019-08-28 13:57:30 +0800

    :recycle: 调整优化代码

--- crashpecker/src/main/java/me/peace/ui/WoodPeckerActivity.java
+++ crashpecker/src/main/java/me/peace/ui/WoodPeckerActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,16 +34,15 @@ public class WoodPeckerActivity extends AppCompatActivity {
     private ArrayList<String> keys = new ArrayList<>();
     private TraceAdapter adapter;
     private String appInfo;
-    private boolean withTrace;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crash_info);
-        obtainIntent();
+        loadConfigParams();
         initView();
-        handleCrashTrace();
         setAppTitleInfo();
+        noProblemTip();
     }
 
     private void setAppTitleInfo(){
@@ -63,66 +61,77 @@ public class WoodPeckerActivity extends AppCompatActivity {
         recyclerView.getViewTreeObserver().addOnGlobalLayoutListener(listener);
     }
 
-    private void obtainIntent(){
+    private void loadConfigParams(){
         Intent intent = getIntent();
         if (intent.hasExtra(Constant.KEY_WITH_TRACE)) {
-            withTrace = true;
+            loadConfigParamsFromIntent(intent);
+        }else{
+            loadConfigParamsFromFile(CrashUtils.getCrashFile(this));
+        }
+    }
+
+    private void loadConfigParamsFromIntent(Intent intent){
+        if (intent != null) {
             list = intent.getStringArrayListExtra(Constant.KEY_TRACES);
             keys = intent.getStringArrayListExtra(Constant.KEY_HIGH_LIGHT);
             appInfo = intent.getStringExtra(Constant.KEY_APP_INFO);
         }
     }
 
-    private void handleCrashTrace(){
-        if (withTrace){
-            noProblemTip();
-        }else{
-            loadLocalTrace();
+    private void loadConfigParamsFromFile(File file){
+        if (file != null){
+            list = loadLocalTraces(CrashUtils.read(file));
+            keys = loadConfigKey();
+            appInfo = loadAppInfo(file);
         }
     }
 
-    private void loadLocalTrace(){
-        File file = CrashUtils.getCrashFile(this);
-        if (file != null) {
-            String trace = CrashUtils.read(file);
-            Log.e(TAG, "withTrace = " + trace);
-            if (!TextUtils.isEmpty(trace)) {
-                String[] traces = trace.split("\n");
-                ArrayList<String> list = new ArrayList<>(Arrays.asList(traces));
-                if (list != null && list.size() > 0) {
-                    loadConfig(file);
-                    this.list = list;
-                    adapter = new TraceAdapter(this.list, this.keys);
-                    recyclerView.setAdapter(adapter);
-                    adapter.notifyDataSetChanged();
-                    return;
-                }
-            }
+    private ArrayList<String> loadLocalTraces(String trace){
+        if (!TextUtils.isEmpty(trace)){
+            String[] traces = trace.split("\n");
+            return new ArrayList<>(Arrays.asList(traces));
         }
-        toast(R.string.app_no_problem);
+        return new ArrayList<>();
     }
 
-    private void loadConfig(File file){
+    private ArrayList<String> loadConfigKey(){
         ProcessDispatcher dispatcher = new ProcessDispatcher();
         String str = dispatcher.loadSpString(this,Constant.WOOD_PECKER_SP,Constant.KEY_HIGH_LIGHT,
             "");
-        keys = Utils.string2List(str);
-
-        String applicationName = dispatcher.loadSpString(this,Constant.WOOD_PECKER_SP,Constant.KEY_APP_NAME,
-            "");
-
-        if (TextUtils.isEmpty(applicationName)){
-            return;
-        }
-
-        String dateTime = getCrashDate(file.getName());
-        if (TextUtils.isEmpty(dateTime)){
-            return;
-        }
-        String title = getResources().getString(R.string.title);
-        appInfo = String.format(title,applicationName,dateTime);
+        return Utils.string2List(str);
     }
 
+    private String loadAppInfo(File file){
+        ProcessDispatcher dispatcher = new ProcessDispatcher();
+        String applicationName = dispatcher.loadSpString(this,Constant.WOOD_PECKER_SP,Constant.KEY_APP_NAME, "");
+        String dateTime = getCrashDate(file.getName());
+        if (!TextUtils.isEmpty(applicationName) && !TextUtils.isEmpty(dateTime)){
+            String title = getResources().getString(R.string.title);
+            return String.format(title,applicationName,dateTime);
+        }
+        return "";
+    }
+
+    private String getCrashDate(String fileName){
+        try {
+            String str = fileName.substring(0,fileName.indexOf("."));
+            String[] strs = str.split("-");
+            return strs[1] + "-" + strs[2] + "-" + strs[3] + " " + strs[4] + ":" + strs[5] + ":" + strs[6];
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private void toast(@StringRes int id){
+        Toast.makeText(this,id,Toast.LENGTH_LONG).show();
+    }
+
+    private void noProblemTip(){
+        if (list != null && list.size() == 0){
+            toast(R.string.app_no_problem);
+        }
+    }
 
     @Override
     protected void onDestroy() {
@@ -170,28 +179,5 @@ public class WoodPeckerActivity extends AppCompatActivity {
             }
         }
     };
-
-    private void toast(@StringRes int id){
-        Toast.makeText(this,id,Toast.LENGTH_LONG).show();
-    }
-
-    private void noProblemTip(){
-        if (withTrace){
-            if (list != null && list.size() == 0){
-                toast(R.string.app_no_problem);
-            }
-        }
-    }
-
-    private String getCrashDate(String fileName){
-        try {
-            String str = fileName.substring(0,fileName.indexOf("."));
-            String[] strs = str.split("-");
-            return strs[1] + "-" + strs[2] + "-" + strs[3] + " " + strs[4] + ":" + strs[5] + ":" + strs[6];
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
 
 }
