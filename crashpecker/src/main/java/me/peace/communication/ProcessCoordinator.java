753a4c503992720e99d0f45d45e2928c77f5316b
peace710 <nothingsuper@163.com>
2019-09-28 17:04:31 +0800

    迁移至AndroidX

--- crashpecker/src/main/java/me/peace/communication/ProcessCoordinator.java
+++ crashpecker/src/main/java/me/peace/communication/ProcessCoordinator.java
@@ -2,7 +2,7 @@ package me.peace.communication;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.text.TextUtils;
 
 import me.peace.utils.SharedPreferencesUtils;
--- crashpecker/src/main/java/me/peace/ui/CrashCacheService.java
+++ crashpecker/src/main/java/me/peace/ui/CrashCacheService.java
@@ -2,7 +2,7 @@ package me.peace.ui;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
